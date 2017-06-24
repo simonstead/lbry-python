5e01f7dee163425fe306d5584805e90cf7b114ce
Simon Stead <simonstead@me.com>
2017-06-24 15:39:15 +0100

    Change folder structure ready for pypi publishing, add setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'lbry',
+    packages = ['lbry'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A client wrapper for the LBRY API, using requests module',
+    author = 'Simon Stead',
+    author_email = 'simonstead@me.com',
+    url = '',
+    download_url = 'download link you saved',
+    keywords = ['lbry', 'crypto'],
+    classifiers = [],
+)
