9f7cbca62ce4b94f4d8c2237cbeaaabe10b04dc3
Simon Stead <simonstead@me.com>
2017-06-24 18:52:38 +0100

    update release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'lbry',
     packages = ['lbry'],
-    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    version = '1.0.1',  # Ideally should be same as your GitHub release tag varsion
     description = 'A client wrapper for the LBRY API, using requests module',
     author = 'Simon Stead',
     author_email = 'simonstead@me.com',
