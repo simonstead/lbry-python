064523f1d9cd5b459f3c3838e295205cc787f9fd
Simon Stead <simonstead@me.com>
2017-06-30 16:56:18 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'lbry',
     packages = ['lbry'],
-    version = '1.0.3',  # Ideally should be same as your GitHub release tag varsion
+    version = '1.0.4',  # Ideally should be same as your GitHub release tag varsion
     description = 'A client wrapper for the LBRY API, using requests module',
     author = 'Simon Stead',
     author_email = 'simonstead@me.com',
     url = 'https://github.com/simonstead/lbry-python',
-    download_url = 'https://github.com/simonstead/lbry-python/archive/v1.0.3.tar.gz',
+    download_url = 'https://github.com/simonstead/lbry-python/archive/v1.0.4.tar.gz',
     keywords = ['lbry', 'crypto'],
     classifiers = [],
 )
